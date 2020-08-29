868be1c7e2f8c1809b374ba80dafe018a8d86dce
Derek Vukusich <dvukusich@gmail.com>
2020-08-29 16:45:45 -0700

    third update - fixing comments

--- src/loops/Loops.java
+++ src/loops/Loops.java
@@ -6,28 +6,33 @@ public class Loops {
 		// TODO Auto-generated method stub
 		//Create a new class called Loops. In the main method of this class, create 
 		//the following loops with any variables you feel are needed:
-		//	
+		
+		
 		// A while loop that prints all even numbers from 0 to 100
-				//int number = 1; 	 
-				//while( number <= 100 ) 
-				//{ 
-					//if( (number % 2) == 0 ) 
-					//{ 
-					//System.out.println(number); 
-				//} 
-				//number++;
-				//}  
-		//
+				int number = 1; 	 
+				while( number <= 100 ) 
+				{ 
+					if( (number % 2) == 0 ) 
+					{ 
+					System.out.println(number); 
+				} 
+				number++;
+				}  
+		
 		// A while loop that prints every 3rd number going backwards from 100 until we reach 0
-				//for (int i = 100; i >=0; i = i - 3) {
-				//System.out.println(i);
-			//}
-		//
+		
+				for (int i = 100; i >= 0; i = i - 3) {
+				System.out.println(i);
+			}
+		
 		// A for loop that prints every other number from 1 to 100
-		//
-				//for (int i = 1; i <= 100; i = i + 2) {
-				//	System.out.println(i);
-				//}
+		
+				
+				for (int i = 1; i <= 100; i = i + 2) {
+					System.out.println(i);
+				}
+				
+				
 		// A for loop that prints every number from 0 to 100, but 
 		// if the number is divisible by 3, it prints “Hello” instead of the 
 		// number, and if the number is divisible by 5, it prints “World” instead 
